44ca20589564bb733c64b38c75dae2b9179ffa4f
Kyle Richardson <kyle.richardson1981@gmail.com>
2017-05-02 18:38:30 -0400

    can now receive data at any time and rehydrate state programmatically

--- demo/index.js
+++ demo/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { ReactWizard, Step } from "../src";
 
-let stateHydration = {
+const stateHydration = {
 	current: 1,
 	steps: {
 		0: {
@@ -27,19 +27,159 @@ const stepFourComponent = (props) => {
 	return <form>Step Four</form>
 }
 
+
+const mockData = [
+	{
+		component: stepOneComponent,
+		title: "Contact Information",
+		subheading: "Please fill out the below contact information so that we can keep you informed about your order!",
+		indicatorLabel: "Contact Information",
+		onNextFunc: (state) => 1
+	},
+	{
+		component: stepTwoComponent,
+		title: "Shipping Information",
+		subheading: "We'll ship your order anywhere in the world!",
+		indicatorLabel: "Shipping Information",
+		onNextFunc: (state) => 2
+	},
+	{
+		component: stepThreeComponent,
+		title: "Billing Information",
+		subheading: "How would you like to pay for your order?",
+		indicatorLabel: "Billing Information"
+	}
+];
+
+let i, len = mockData.length, steps = new Array(), step;
+
+for (i = 0; i < len; i++) {
+	step = mockData[i];
+	steps = steps.concat(
+		<Step
+			key={ "myWizardStep_" + i }
+			component={ step.component({}) }
+			title={ step.title }
+			subheading={ step.subheading }
+			indicatorLabel={ step.indicatorLabel }
+			onNextFunc={ step.onNextFunc }
+		/>
+	)
+}
+
+
+const mockData1 = [
+	{
+		component: stepOneComponent,
+		title: "Contact1 Information",
+		subheading: "Please fill out the below contact information so that we can keep you informed about your order!",
+		indicatorLabel: "Contact Information",
+		onNextFunc: (state) => 1
+	},
+	{
+		component: stepTwoComponent,
+		title: "Shipping1 Information",
+		subheading: "We'll ship your order anywhere in the world!",
+		indicatorLabel: "Shipping Information",
+		onNextFunc: (state) => 2
+	},
+	{
+		component: stepThreeComponent,
+		title: "Billing1 Information",
+		subheading: "How would you like to pay for your order?",
+		indicatorLabel: "Billing Information"
+	}
+];
+
+let i1, len1 = mockData1.length, steps1 = new Array(), step1;
+
+for (i1 = 0; i1 < len1; i1++) {
+	step1 = mockData1[i1];
+	steps1 = steps1.concat(
+		<Step
+			key={ "myWizardStep_" + i1 }
+			component={ step1.component({}) }
+			title={ step1.title }
+			subheading={ step1.subheading }
+			indicatorLabel={ step1.indicatorLabel }
+			onNextFunc={ step1.onNextFunc }
+		/>
+	)
+}
+
+const stateHydration1 = {
+	current: 2,
+	steps: {
+		0: {
+			complete: true
+		},
+		1: {
+			complete: true,
+			warning: true
+		}
+	}
+}
+
 class Root extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			awaitingData: true
+		}
+
+		this.steps = null;
+		this.stateHydration = null;
+
+		this.injectData = this.injectData.bind(this);
+		this.injectDataNewData = this.injectDataNewData.bind(this);
+	}
+
+	injectData() {
+		this.steps = steps;
+		this.stateHydration = stateHydration;
+		this.setState({
+			...this.state,
+			awaitingData: false
+		});
+	}
+
+	injectDataNewData() {
+		this.steps = steps1;
+		this.stateHydration = stateHydration1;
+		this.setState({
+			...this.state,
+			awaitingData: false
+		}, this.wizard.rehydrateState);
 	}
 
 	render() {
 		return (
 			<div>
 				<ReactWizard
-					//hydrateState={ stateHydration }
+					ref={ (wiz) => this.wizard = wiz }
+					hydrateState={ this.stateHydration }
 					//onComplete={}
 					//afterCompleteComponent={}
 					>
+					{ this.steps }
+				</ReactWizard>
+				<br />
+				<button type="button" onClick={ this.injectData }>Inject data</button>
+				<button type="button" onClick={ this.injectDataNewData }>Inject new data</button>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Root />, document.getElementById("app"));
+
+
+
+
+/*
+
+
 					<Step
 						component={ stepOneComponent({}) }
 						title="Contact Information"
@@ -88,10 +228,6 @@ class Root extends Component {
 						//nextLabel=""
 						//onNextFunc={}
 					/>
-				</ReactWizard>
-			</div>
-		);
-	}
-}
 
-ReactDOM.render(<Root />, document.getElementById("app"));
+
+*/
--- src/react-wizard/index.js
+++ src/react-wizard/index.js
@@ -1,214 +1,54 @@
-import React, { Component } from "react";
-import ReactWizard from "./react-wizard.presentational";
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import ReactWizardContainer from "./react-wizard.container";
 
 import "./styles.scss";
 
-class ReactWizardContainer extends Component {
+class ConditionalRenderWrapper extends PureComponent {
 	constructor(props) {
 		super(props);
-
-		this.state = _hydrateState(this.props.hydrateState, this.props.children);
-
-		this.prev = this.prev.bind(this);
-		this.next = this.next.bind(this);
-		this.renderChildren = this.renderChildren.bind(this);
 	}
 
-	componentWillMount() {
-		// determine the type of controls each step needs based on its index
-		this.childrenWithControls = React.Children.map(this.props.children,
-			(child, index) => React.cloneElement(child, {
-				index: index,
-				_onPrevFunc: this.prev,
-				_onNextFunc: this.next,
-				controls: _createControls(child.props,	index, ( this.props.children.length - 1 )),
-				current: this.state.current,
-				complete: this.state.steps[index].complete,
-				warning: this.state.steps[index].warning,
-				error: this.state.steps[index].error,
-				disableNext: this.state.steps[index].disableNext
-			})
-		);
-
-		this.indicatorProperties = new Array();
-
-		let i, len = this.props.children.length;
-
-		for (i = 0; i < len; i++) {
-			this.indicatorProperties = this.indicatorProperties.concat({
-				id: this.props.children[i].props.indicatorId,
-				label: this.props.children[i].props.indicatorLabel,
-				iconClasses: this.props.children[i].props.indicatorIconClasses
-			});
+	rehydrateState() {
+		if (this.wizard) {
+			this.wizard.rehydrateState();
 		}
-	}
-
-	prev(onPrevCallback) {
-		let proceed = true;
-
-		if (onPrevCallback) {
-			proceed = onPrevCallback(this.state);
-		}
-
-		this.setState({
-			...this.state,
-			current: proceed ? this.state.current - 1 : this.state.current
-		});
-	}
-
-	next(validationFunc, finalStep) {
-		let newState, validationResponse = typeof validationFunc === "function" ? validationFunc(this.state) : 1;
-
-		// case 0 means validation failed
-		// case 1 means validation passed
-		// case 2 means validation returned a warning
-
-		switch (validationResponse) {
-			case 0:
-				newState = _newState(this.state, 0, { complete: false, warning: false, error: true, disableNext: true });
-				break;
-			case 1:
-				newState = _newState(this.state, 1, { complete: true, warning: false, error: false, disableNext: false });;
-				break;
-			case 2:
-				newState = _newState(this.state, 1, { complete: true, warning: true, error: false, disableNext: false });;
-				break;
-		}
-
-		this.setState(newState);
-	}
-
-	renderChildren() {
-		return React.Children.map(this.childrenWithControls,
-			(child, index) => {
-				return React.cloneElement(child, {
-					current: this.state.current,
-					complete: this.state.steps[index].complete,
-					warning: this.state.steps[index].warning,
-					error: this.state.steps[index].error,
-					disableNext: this.state.steps[index].disableNext
-				})
-			}
-		);
 	}
 
 	render() {
-		let { children, ...rest } = this.props;
-
-		return (
-			<ReactWizard
-				indicatorProperties= { this.indicatorProperties }
-				steps={ this.state.steps }
-				currentStep={ this.state.current }
-				currentStepTitle={ children[this.state.current].props.title }
-				currentStepSubheading={ children[this.state.current].props.subheading }>
-				{ this.renderChildren() }
-			</ReactWizard>
-		);
-	}
-}
-
-export default ReactWizardContainer;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Internal functions, methods, and variables */
-
-function _hydrateState(stateHydration, steps) {
-	let defaultStepState = {
-		complete: false,
-		warning: false,
-		error: false,
-		disableNext: false
-	}
-
-	let _steps = React.Children.map(steps,
-		(child, index) => {
-			if (!stateHydration) {
-				return defaultStepState;
-			} else {
-				if (stateHydration.steps) {
-					if (stateHydration.steps[index]) {
-						return {
-							complete: stateHydration.steps[index].complete || false,
-							warning: stateHydration.steps[index].warning || false,
-							error: stateHydration.steps[index].error || false,
-							disableNext: stateHydration.steps[index].disableNext || false
-						}
+		let { id, children, loadingSpinner, hydrateState, onComplete, afterCompleteComponent, ...rest } = this.props;
+		if (children) {
+			return (
+				<ReactWizardContainer
+					ref={ (wizard) => this.wizard = wizard }
+					id={ id }
+					hydrateState={ hydrateState }
+					onComplete={ onComplete }
+					afterCompleteComponent={ afterCompleteComponent }>
+					{ children }
+				</ReactWizardContainer>
+			);
+		} else {
+			return (
+				<div className="react-wizard">
+					<div className="react-wizard-header" />
+					{
+						loadingSpinner ?
+							loadingSpinner :
+							<div className="react-wizard-spinner" />
 					}
-					return defaultStepState;
-				}
-				return defaultStepState;
-			}
+				</div>
+			);
 		}
-	);
-
-	return { steps: _steps, current: stateHydration ? stateHydration.current : 0 };
-}
-
-function _createControls(childProps, index, endingIndex) {
-	// control.type number representation:
-	// 0: prev
-	// 1: next
-	// 2: complete
-
-	let controls;
-	switch (index) {
-		case 0:
-			controls = [ _createControl(1, childProps.nextLabel || "NEXT", childProps.onNextFunc) ];
-			break;
-		case endingIndex:
-			controls = [
-			_createControl(0, childProps.prevLabel || "PREVIOUS", childProps.onPrevFunc),
-			_createControl(	2,
-							childProps.completeLabel || childProps.nextLabel || "COMPLETE",
-							childProps.onCompleteFunc || childProps.onNextFunc)
-		];
-			break;
-		default:
-			controls = [
-				_createControl(0, childProps.prevLabel || "PREVIOUS", childProps.onPrevFunc),
-				_createControl(1, childProps.nextLabel || "NEXT", childProps.onNextFunc)
-			];
-			break;
-	}
-
-	return controls;
-}
-
-function _createControl(type, label, validationFunc) {
-	return {
-		type: type,
-		label: label,
-		validationFunc: validationFunc
 	}
 }
 
-function _newState(state, changeInCurrentlyActiveStep, currentStepNewStatus) {
-	return {
-		...state,
-		current: state.current + changeInCurrentlyActiveStep,
-		steps: state.steps.map((step, index) => {
-			if (index !== state.current) {
-				return step;
-			}
-
-			return currentStepNewStatus;
-		})
-	}
+ConditionalRenderWrapper.propTypes = {
+	id: PropTypes.string,
+	loadingSpinner: PropTypes.element,
+	hydrateState: PropTypes.object,
+	onComplete: PropTypes.func,
+	afterCompleteComponent: PropTypes.element
 }
+
+export default ConditionalRenderWrapper;
